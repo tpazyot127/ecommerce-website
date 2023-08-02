a148cfd91bf6f72ae06c7f1f9bf53db40db382e9
Edward <tpazyot127@gmail.com>
2023-08-02 16:08:49 +0700

    configured data

--- client/config/index.js
+++ client/config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev 
     ? "http://localhost:4000" 
-    : "https://nest-nextjs.vercel.app";
+    : "https://ocop-backend.fly.dev";
--- server/src/main.ts
+++ server/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.enableCors(corsConfig());
   app.use(session(sessionConfig(MongoDBStore)));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
