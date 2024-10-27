d1a6f98897cd7ce79b12223b5eaf22da2c81760e
khoathuc <khoathuc141201@gmail.com>
2024-10-27 13:48:24 +0700

    sf

--- server/src/products/controller/products.controller.ts
+++ server/src/products/controller/products.controller.ts
@@ -1,20 +1,18 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Post,
-    Put,
-    Query,
-    Session,
-    UploadedFiles,
-    UseGuards,
-    UseInterceptors,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Session,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
 } from "@nestjs/common";
-import {
-    FilesInterceptor,
-} from "@nestjs/platform-express";
+import { FilesInterceptor } from "@nestjs/platform-express";
 import { AdminGuard } from "src/guards/admin.guard";
 import { AuthGuard } from "src/guards/auth.guard";
 import { ProductDto } from "../dtos/product.dto";
@@ -25,98 +23,98 @@ import { Image } from "../schemas/product.schema";
 
 @Controller("products")
 export class ProductsController {
-    constructor(
-        private productsService: ProductsService,
-        private appService: AppService
-    ) {}
+  constructor(
+    private productsService: ProductsService,
+    private appService: AppService
+  ) {}
 
-    @Get()
-    getProducts(
-        @Query("keyword") keyword: string,
-        @Query("pageId") pageId: string
-    ) {
-        return this.productsService.findMany(keyword, pageId);
-    }
+  @Get()
+  getProducts(
+    @Query("keyword") keyword: string,
+    @Query("pageId") pageId: string
+  ) {
+    return this.productsService.findMany(keyword, pageId);
+  }
 
-    @Get("topRated")
-    getTopRatedProducts() {
-        return this.productsService.findTopRated();
-    }
+  @Get("topRated")
+  getTopRatedProducts() {
+    return this.productsService.findTopRated();
+  }
 
-    @Get(":id")
-    getProduct(@Param("id") id: string) {
-        return this.productsService.findById(id);
-    }
+  @Get(":id")
+  getProduct(@Param("id") id: string) {
+    return this.productsService.findById(id);
+  }
 
-    @UseGuards(AdminGuard)
-    @Delete(":id")
-    deleteUser(@Param("id") id: string) {
-        return this.productsService.deleteOne(id);
-    }
+  @UseGuards(AdminGuard)
+  @Delete(":id")
+  deleteUser(@Param("id") id: string) {
+    return this.productsService.deleteOne(id);
+  }
 
-    @Post("create")
-    @UseInterceptors(FilesInterceptor("images"))
-    async createProduct(
-        @Body() body: any,
-        @UploadedFiles() images: Express.Multer.File[]
-    ) {
-        const imageUrls: Image[] = await Promise.all(
-            images.map(async (image) => {
-                const res = await this.appService.uploadImageToCloudinary(
-                    image
-                );
-
-                return { img: res.url };
-            })
+  @Post("create")
+  @UseInterceptors(FilesInterceptor("images"))
+  async createProduct(
+    @Body() body: any,
+    @UploadedFiles() images: Express.Multer.File[]
+  ) {
+    const imageUrls: Image[] = await Promise.all(
+      images.map(async (image) => {
+        const res = await this.appService.uploadImageToCloudinary(
+            image
         );
 
-        return this.productsService.create({
-            ...body,
-            images: imageUrls,
-        });
-    }
+        return { img: res.url };
+      })
+    );
 
-    @Post("createMany")
-    createProducts(@Body() products: any) {
-        return this.productsService.createMany(products);
-    }
+    return this.productsService.create({
+      ...body,
+      images: imageUrls,
+    });
+  }
 
-    @UseGuards(AdminGuard)
-    @Put(":id")
-    @UseInterceptors(FilesInterceptor("images"))
-    async updateProduct(
-        @Param("id") id: string,
-        @Body() product: ProductDto,
-        @UploadedFiles() images: Express.Multer.File[]
-    ) {
-        const imageUrls: Image[] = await Promise.all(
-            images.map(async (image) => {
-                const res = await this.appService.uploadImageToCloudinary(
-                    image
-                );
+  @Post("createMany")
+  createProducts(@Body() products: any) {
+    return this.productsService.createMany(products);
+  }
 
-                return { img: res.url };
-            })
+  @UseGuards(AdminGuard)
+  @Put(":id")
+  @UseInterceptors(FilesInterceptor("images"))
+  async updateProduct(
+    @Param("id") id: string,
+    @Body() product: ProductDto,
+    @UploadedFiles() images: Express.Multer.File[]
+  ) {
+    const imageUrls: Image[] = await Promise.all(
+      images.map(async (image) => {
+        const res = await this.appService.uploadImageToCloudinary(
+          image
         );
 
-        return this.productsService.update(id, {
-            ...product,
-            images: imageUrls,
-        });
-    }
+        return { img: res.url };
+      })
+    );
 
-    @UseGuards(AuthGuard)
-    @Put(":id/review")
-    createReview(
-        @Param("id") id: string,
-        @Body() { rating, comment }: ReviewDto,
-        @Session() session: any
-    ) {
-        return this.productsService.createReview(
-            id,
-            session.user,
-            rating,
-            comment
-        );
-    }
+    return this.productsService.update(id, {
+      ...product,
+      images: imageUrls,
+    });
+  }
+
+  @UseGuards(AuthGuard)
+  @Put(":id/review")
+  createReview(
+    @Param("id") id: string,
+    @Body() { rating, comment }: ReviewDto,
+    @Session() session: any
+  ) {
+    return this.productsService.createReview(
+      id,
+      session.user,
+      rating,
+      comment
+    );
+  }
 }
